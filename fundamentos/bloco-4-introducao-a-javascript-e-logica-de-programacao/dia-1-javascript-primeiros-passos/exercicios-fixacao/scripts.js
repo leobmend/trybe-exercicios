33d4846807f2109ec08e0664cf90549ca13e9d6c
Leonardo Mendonca <leo.bmendonca@gmail.com>
2021-11-05 17:21:44 -0300

    Exercicio fixacao m1b4d1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-fixacao/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-fixacao/scripts.js
@@ -1,4 +1,4 @@
-/* Variáveis */
+/* VARIÀVEIS */
 
 const myName = "Leonardo"
 const birthCity = "Anápolis"
@@ -21,7 +21,7 @@ birthCity = "Tokio"
 console.log(birthCity)
     O erro TypeError: Assignment to constant variable indica que tentamos alterar o valor definida para a constante birthCity anteriormente. E, por ser uma constante, não é acecito tal ação.*/
     
-/* Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas */
+/* TIPOS PRIMITIVOS, TIPAGEM DINÂMICA E OPERAÇÔES ARITMÈTICAS */
 
 let patientId = 50;
 let isEnrolled = true;
@@ -36,31 +36,71 @@ console.log(typeof isEnrolled);
 console.log(typeof patientInfo);
 console.log(typeof patientEmail);
 
-console.log("\n")
+console.log("\n");
 
 console.log(typeof patientAge);
 
-console.log("\n")
+console.log("\n");
 
 patientId = '50';
 console.log(typeof patientId);
 
-console.log("\n")
+console.log("\n");
 
-/* Crie uma costante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8. */
+/* 1. Crie uma costante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8. */
 
 const base = 5;
 let height = 8;
-console.log(base + " x " + height)
+console.log(base + " x " + height);
 
-/* Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações! */
+/* 2. Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações! */
 
 console.log("\n")
 const area = base * height;
 console.log(area)
 
-/* Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo. */
+/* 3. Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo. */
+
+console.log("\n");
+const perimeter = 2 * base + 2 * height;
+console.log(perimeter);
+
+/* CONDIÇÔES IF E ELSE */
+
+/* 1. Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100; */
+
+console.log("\n");
+const nota = 75;
+console.log(nota);
+
+/* 2. Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações:
+Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
+Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera"
+Se a nota for menor que 60, imprima "Você foi reprovada(o)" 
+3. Crie uma estrutura condicional utilizando o if , else if e else para criar o seu algoritmo, e os operadores lógicos que se aplicam a cada situação.*/
 
 console.log("\n")
-const perimeter = 2 * base + 2 * height;
-console.log(perimeter)
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada");
+} 
+else if(nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovada(o)");
+}
+    
+/* 4. Altere o valor da nota para verificar se as condições que você implementou funcionam; */
+
+console.log("\n");
+const nota2 = 50;
+console.log("\n")
+if (nota2 >= 80) {
+    console.log("Parabéns, você foi aprovada");
+} 
+else if(nota2 < 80 && nota2 >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovada(o)");
+}
