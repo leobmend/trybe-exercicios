f032534d8256737b6431d579e58a052d3688a8e4
Leonardo Mendonca <leo.bmendonca@gmail.com>
2021-11-05 19:26:10 -0300

    Exercicios na pratica m1b4d1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-fixacao/ex-7.js
@@ -0,0 +1,18 @@
+let nota = 72;
+console.log(nota);
+
+if (nota < 50) {
+    nota = "F";
+} else if (nota < 60) {
+    nota = "E";
+} else if (nota < 70) {
+    nota = "D";
+} else if (nota < 80) {
+    nota = "C";
+} else if (nota < 90) {
+    nota = "B";
+} else {
+    nota = "A";
+}
+
+console.log(nota);
