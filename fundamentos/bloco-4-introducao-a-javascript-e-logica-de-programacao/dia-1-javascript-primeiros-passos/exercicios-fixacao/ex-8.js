16d9478bb472eac8cbecc7b44cd93ce2d4c51508
Leonardo Mendonca <leo.bmendonca@gmail.com>
2021-11-05 20:01:35 -0300

    Exercicios na pratica m1b4d1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-fixacao/ex-8.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-fixacao/ex-8.js
@@ -1,10 +1,10 @@
-const a = 2;
+const a = 5;
 const b = 7;
 const c = 11;
 
-soma = a + b + c;
+const restos = (a%2) + (b%2) + (c%2);
 
-if (soma % 2 == 0) {
+if (restos < 3) {
     console.log(true)
 } else {
     console.log(false)
