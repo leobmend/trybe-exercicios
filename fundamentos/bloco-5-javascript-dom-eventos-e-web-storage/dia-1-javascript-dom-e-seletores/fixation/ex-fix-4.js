4d677e7a0b105b121f3636d13f2541e548f1eb8d
Leonardo Mendonca <leo.bmendonca@gmail.com>
2021-11-16 18:40:19 -0300

    Ex fixacao 4 m1b5d1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fixation/ex-fix-4.js
@@ -0,0 +1,23 @@
+/* Vamos consolidar a manipulação dos elementos HTML , colocando a cor do Administrador de Tempo da Trybe como na imagem ex-fix-4.png presente na atual pasta.
+Você vai precisar usar o que aprendeu sobre getElementBy e querySelector para colocar em prática. */
+
+const header = document.getElementById("header-container");
+header.style.backgroundColor = "#00B069";
+
+const urgentSubtitles = document.querySelectorAll(".emergency-tasks h3");
+for (let urgentSubtitle of urgentSubtitles) {
+    urgentSubtitle.style.backgroundColor = "#A500F3";
+}
+
+const urgentSection = document.querySelector(".emergency-tasks");
+urgentSection.style.backgroundColor = "#FD9B87"
+
+const noUrgentSubtitles = document.querySelectorAll(".no-emergency-tasks h3");
+for (let noUrgentSubtitle of noUrgentSubtitles) {
+    noUrgentSubtitle.style.backgroundColor = "#565033";
+}
+
+const noUrgentSection = document.querySelector(".no-emergency-tasks");
+noUrgentSection.style.backgroundColor = "#F9DB5E"
+
+
