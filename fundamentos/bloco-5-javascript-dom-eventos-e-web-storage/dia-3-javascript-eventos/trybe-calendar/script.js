98e970e0447a3f9aa5a6e73d8164ff3d05c44ccb
Leonardo Mendonca <leo.bmendonca@gmail.com>
2021-11-21 16:08:09 -0300

    Ex 8 trybe-calendar m1b5d3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/trybe-calendar/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/trybe-calendar/script.js
@@ -139,3 +139,16 @@ function addTask(taskString) {
     myTasks.appendChild(task);
 }
 addTask("Cozinhar");
+
+/* Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+- O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+- O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" . */
+
+function addSubtitle(color) {
+    let myTasks = document.getElementsByClassName("my-tasks")[0];
+    let subtitle = document.createElement("div");
+    subtitle.className = "task";
+    subtitle.style.backgroundColor = color;
+    myTasks.appendChild(subtitle);
+}
+addSubtitle("yellow");
