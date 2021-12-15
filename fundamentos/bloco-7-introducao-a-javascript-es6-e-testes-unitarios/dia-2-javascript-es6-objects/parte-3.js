b29469292281456b4a90890b8821bbfcae78d796
Leonardo Mendonca <leo.bmendonca@gmail.com>
2021-12-15 15:33:19 -0300

    Ex 5, na-pratica parte 3, m1b7d2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte-3.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte-3.js
@@ -53,10 +53,17 @@ objectSize(lesson3) */
 4. Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 */
 
-function printObjValues(object) {
+/* function printObjValues(object) {
   const values = Object.values(object);
   for (let i = 0; i < values.length; i += 1) {
     console.log(`${i + 1}. ${values[i]}`);
   }
 }
-printObjValues(lesson1);
+printObjValues(lesson1); */
+
+/* 
+5. Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+*/
+
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+console.log(allLessons);
