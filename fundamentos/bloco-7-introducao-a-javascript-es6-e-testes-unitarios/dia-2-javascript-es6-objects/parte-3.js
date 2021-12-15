1eaf0d8364ac0950c03f21082249bd3ace8fe734
Leonardo Mendonca <leo.bmendonca@gmail.com>
2021-12-15 15:51:27 -0300

    Ex 8, na-pratica parte 3, m1b7d2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte-3.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte-3.js
@@ -85,9 +85,25 @@ console.log(allStudents(allLessons)); */
 7. Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
 */
 
-function getValueByPosition(object, position) {
+/* function getValueByPosition(object, position) {
   let objectKeys = Object.keys(object);
   return object[objectKeys[position]];
 }
-console.log(getValueByPosition(lesson1, 2));
+console.log(getValueByPosition(lesson1, 2)); */
 
+/* 
+8. Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
+*/
+
+function verifyPair(object, key, value) {
+  const objectKeys = Object.keys(object);
+  if (!objectKeys.includes(key)) {
+    return false;
+  } else if (object[key] != value) {
+    return false;
+  } else {
+    return true;
+  }
+}
+console.log(verifyPair(lesson3, 'turno','noite'));
+console.log(verifyPair(lesson3, 'materia', 'Maria Clara'))
