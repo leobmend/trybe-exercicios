19803dfddba82bbfa8293241112582ccfb16edad
Leonardo Mendonca <leo.bmendonca@gmail.com>
2021-12-29 10:39:47 -0300

    Ex 6.1 e 6.2 na-pratica, m1b7d3

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/ex-6.js
@@ -0,0 +1,24 @@
+/* Exercício 6
+  - A partir dos testes contidos no arquivo ex-6.text.js, construa a função hydrate. 
+  - A função hydrate recebe uma string no formato "numero bebida", e retorna a sugestão de quantos copos de água você deve beber para se hidratar. 
+    / string recebida
+    '1 cerveja'
+    / retorno da função
+    '1 copo de água'
+
+    / string recebida
+    '1 cerveja, 2 shots e 1 catuaba'
+    / retorno da função
+    '4 copos de água'
+
+    / string recebida
+    '2 caipirinhas'
+    / retorno da função
+    '2 copos de água'
+*/
+
+function hydrate() {
+
+};
+
+module.exports = hydrate;
