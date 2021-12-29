75229e07573c1595cd2771419ca9be86224fdfc2
Leonardo Mendonca <leo.bmendonca@gmail.com>
2021-12-29 10:07:40 -0300

    Ex 5.1 e 5.2 na-pratica, m1b7d3

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/ex-5.js
@@ -0,0 +1,14 @@
+/* Exercício 5
+  - A partir dos testes contidos no arquivo ex-5.text.js, construa a função techList. 
+  - A função techList recebe como parâmetros um array contendo uma lista de tecnologias e uma string com um nome. Para cada tecnologia no array a função cria, em ordem alfabética, um objeto com a seguinte estrutura:
+    {
+      tech: 'nomeTecnologia',
+      name: name,
+    }
+*/
+
+function techList() {
+
+};
+
+module.exports = techList;
