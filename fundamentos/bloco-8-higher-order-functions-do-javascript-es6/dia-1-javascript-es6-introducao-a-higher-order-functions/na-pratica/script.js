4af81dac7fc2878c788aee53098f677a5c59f809
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-01-03 00:38:41 -0300

    Ex 3 na-pratica, m1b8d1

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/na-pratica/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/na-pratica/script.js
@@ -36,4 +36,30 @@ const raffle = (bet, callback) => {
   }
 }
 
-console.log(raffle(5, checkEquality));
+// console.log(raffle(5, checkEquality));
+
+/* 
+  3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+    - Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+*/
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const gradeCounter = (list1, list2) => {
+  let counter = 0;
+  for (let index = 0; index < list1.length; index += 1) {
+    if (list1[index] !== list2[index]) {
+      counter += 1;
+    } else if (list1[index] !== list2[index] && list1[index] !== 'N.A') {
+      counter -= 0.5;
+    }
+  }
+  return counter;
+}
+
+const examGrader = (student_answers, right_answers, callback) => {
+  return callback(student_answers, right_answers);
+}
+
+console.log(examGrader(STUDENT_ANSWERS, RIGHT_ANSWERS, gradeCounter))
