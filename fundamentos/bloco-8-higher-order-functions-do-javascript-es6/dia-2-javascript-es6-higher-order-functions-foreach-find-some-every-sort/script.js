bde8e470685b74a172821ed6b869156d81c4dc3e
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-01-04 10:17:10 -0300

    Ex 4-5, para-fixar, m1b8d2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
@@ -34,3 +34,33 @@ const names = ['João', 'Irene', 'Fernando', 'Maria'];
 const findNameWithFiveLetters = () => names.find(name => name.length === 5)
 
 //console.log(findNameWithFiveLetters());
+
+/* 
+  4 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false , use some ;
+*/
+
+const names2 = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  return arr.some(item => item === name);
+}
+
+//console.log(hasName(names2, 'Ana'))
+
+/* 
+  5 - Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false , use every ;
+*/
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every(item => item.age >= 18);
+}
+
+//console.log(verifyAges(people, 18));
