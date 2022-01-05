834aa568d892e2c6a9ae40545dc9054b6a864e07
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-01-05 10:41:01 -0300

    Ex 7, na-pratica, m1b8d3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise7.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise7.js
@@ -62,37 +62,15 @@ const books = [
 ];
 
 /* 
-  4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
-  - Dica: use as funções filter e sort
+  7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+  - Dica: cada inicial termina com um ponto.
 */
 
-const expectedResult = [
-  {
-    id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
-    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
-    releaseYear: 1928,
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954,
-  },
-];
+const expectedResult = 'O Senhor dos Anéis';
 
-function oldBooksOrdered() {
-  const books60Years = books.filter(book => (2022 - book.releaseYear) > 60);
-  books60Years.sort((bookA, bookB) => bookA.releaseYear - bookB.releaseYear);
-  console.log(books60Years)
+function authorWith3DotsOnName() {
+  const book3DotsAuthor = books.find(book => book.author.name.split('.').length === 4);
+  console.log(book3DotsAuthor.name);
 }
-oldBooksOrdered();
+
+authorWith3DotsOnName();
