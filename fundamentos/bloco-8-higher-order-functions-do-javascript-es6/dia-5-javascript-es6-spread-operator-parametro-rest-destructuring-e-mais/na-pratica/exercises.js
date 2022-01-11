02e63c6cf44dd89bbd4cafe6bc47b1003cb0d8e3
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-01-11 10:33:39 -0300

    Ex 6, 7 e 8 na-pratica, m1b8d4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/na-pratica/exercises.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/na-pratica/exercises.js
@@ -86,4 +86,57 @@ const myList = [1, 2, 3];
 
 // escreva swap abaixo
 const swap = ([n1, n2, n3]) => [n3, n2, n1];
-console.log(swap(myList));
+
+// console.log(swap(myList));
+
+// 6 - Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, cada carro é modelado como um array. Porém, essa modelagem está baixo nível. Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
+// Dica: use array destructuring e abbreviation object literal.
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+const toObject = ([carModel, carManufactor, carYear]) => ({
+  carModel, carManufactor, carYear
+});
+
+// console.log(toObject(palio));
+
+// 7 - Escreva uma função shipLength que, dado um objeto representando um navio, retorna o comprimento dele, mostrando também a devida unidade de comprimento:
+// Dica: use object destructuring.
+
+const ships = [
+  {
+    name: 'Titanic',
+    length: 269.1,
+    measurementUnit: 'meters',
+  },
+  {
+    name: 'Queen Mary 2',
+    length: 1132,
+    measurementUnit: 'feet',
+  },
+  {
+    name: 'Yamato',
+    length: 256,
+    measurementUnit: 'meters',
+  },
+];
+
+// escreva shipLength abaixo
+const shipLength = ({ name, length, measurementUnit }) => `${name} is ${length} ${measurementUnit} long`;
+
+// console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
+// console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
+// console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
+
+// 8 - Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento:
+// Dica: use default params.
+
+// escreva greet abaixo
+const greet = (name, greeting = 'Hi') => `${greeting} ${name}`;
+
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
