cc33a55f6426967c30aaf768395efd82dde4fc48
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-01-11 10:18:36 -0300

    Ex 4 e 5 na-pratica, m1b8d4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/na-pratica/exercises.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/na-pratica/exercises.js
@@ -39,5 +39,51 @@ const gunnar = {
 // complete a assinatura da função abaixo
 const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
 
-console.log(personLikes(alex)) // 'Alex is 26 years old and likes fly fishing.'
-console.log(personLikes(gunnar)) // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+// console.log(personLikes(alex)) // 'Alex is 26 years old and likes fly fishing.'
+// console.log(personLikes(gunnar)) // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+// 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+// Dica: use object destructuring.
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+const filterPeople = (peopleList) => peopleList.filter(({ nationality, bornIn }) => (
+  nationality === 'Australian' && bornIn <= 2000 && bornIn > 1900
+));
+
+// console.log(filterPeople(people));
+
+// 5 - Escreva a função swap , que dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando 1 linha só:
+// Dica: use array destructuring.
+
+const myList = [1, 2, 3];
+
+// escreva swap abaixo
+const swap = ([n1, n2, n3]) => [n3, n2, n1];
+console.log(swap(myList));
