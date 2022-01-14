87da9ae4034bb1ba6d7dd901d017cdbe4e082705
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-01-14 09:58:22 -0300

    Ex 2, na-pratica, m1b9d2

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/na-pratica/ex-2.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/na-pratica/ex-2.js
@@ -1,9 +1,22 @@
 const url = 'https://api.coincap.io/v2/assets';
 
-function addCryptos(cryptoObj) {
-  const cryptoIl = document.createElement('li');
-  cryptoIl.innerText = cryptoObj.id
-  cryptoList.appendChild(cryptoIl)
+function addCoin(coin) {
+  const coinsList = document.querySelector('main');
+  
+  const coinDiv = document.createElement('div');
+  coinDiv.className = 'coin-div';
+  
+  const coinName = document.createElement('h2');
+  coinName.className = 'coin-id';
+  coinName.innerText = coin.name;
+
+  const coinPrice = document.createElement('p');
+  coinPrice.className = 'coin-price';
+  coinPrice.innerText = `US$ ${parseFloat(coin.priceUsd).toFixed(3)}`;
+
+  coinDiv.appendChild(coinName);
+  coinDiv.appendChild(coinPrice);
+  coinsList.appendChild(coinDiv)
 }
 
 function fetchCoins(callback) {
@@ -11,13 +24,14 @@ function fetchCoins(callback) {
   fetch(url, resquestOptions)
     .then(response => response.json())
     .then(data => {
-      Object.keys(data.data).forEach((index) => {
-        const cryptoObj = data.data[index]
-        callback(cryptoObj);
-      })
-      // data.data
-      // addCryptos(data.data, 5);
+      Object.keys(data.data)
+      .filter((index) => index < 25)
+      .forEach((index) => {
+          const coinObj = data.data[index]
+          callback(coinObj);
+        })
+      console.log(data.data)
     })
 }
 
-window.onload = fetchCoins(addCryptos);
+window.onload = fetchCoins(addCoin);
