05f9defd9d66c2d994b43a9dcfa6e30ce0f6a8df
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-01-24 15:27:07 -0300

    Ex 4, fixacao, m2b10d2

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixacao/fixation-ex-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixacao/fixation-ex-10-2/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 // import Image from './Image';
 // import Album from './Album';
-import Order from './Order';
+// import Order from './Order';
+import UserProfile from './UserProfile';
 import React from 'react';
 
 // Ex 1
@@ -56,34 +57,60 @@ import React from 'react';
 //   }
 // }
 
-function App() {
-  const headphone = {
-    id: 102,
-    user: "cena@gmail.com",
-    product: "Razer Headphone",
-    price: {
-      value: 99.99,
-      currency: "dollars"
-    }
-  };
+// Ex 3
+// function App() {
+//   const headphone = {
+//     id: 102,
+//     user: "cena@gmail.com",
+//     product: "Razer Headphone",
+//     price: {
+//       value: 99.99,
+//       currency: "dollars"
+//     }
+//   };
 
-  const energyDrink = {
-    id: 77,
-    user: "cena@gmail.com",
-    product: "Monster 500mL",
-    price: {
-      value: 9.99,
-      currency: "dollars"
-    }
-  };
+//   const energyDrink = {
+//     id: 77,
+//     user: "cena@gmail.com",
+//     product: "Monster 500mL",
+//     price: {
+//       value: 9.99,
+//       currency: "dollars"
+//     }
+//   };
 
-  return (
-    <div className="App">
-      <h1> Orders recently created </h1>
-      <Order order={headphone} />
-      <Order order={energyDrink} />
-    </div>
-  );
+//   return (
+//     <div className="App">
+//       <h1> Orders recently created </h1>
+//       <Order order={headphone} />
+//       <Order order={energyDrink} />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    const users = [
+      {
+        id: 102,
+        name: "João",
+        email: "joao@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+      },
+      {
+        id: 77,
+        name: "Amélia",
+        email: "amelia@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+      }
+    ];
+
+    return (
+      <div className="App">
+        {users.map((user, index) => <UserProfile key={index} user={user}/>)}
+      </div>
+    );
+  }
 }
 
 export default App;
