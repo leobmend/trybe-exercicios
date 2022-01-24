b5e02047a9fd191db64b6a6b4f6f7a42c37b592a
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-01-24 15:07:47 -0300

    Ex 3, fixacao, m2b10d2

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixacao/fixation-ex-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixacao/fixation-ex-10-2/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 // import Image from './Image';
-import Album from './Album';
+// import Album from './Album';
+import Order from './Order';
 import React from 'react';
 
 // Ex 1
@@ -12,46 +13,77 @@ import React from 'react';
 //   // );
 // }
 
-class App extends React.Component {
-  render() {
-    // Declaração do objeto contendo informações do album01
-    const album01 = {
-      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
-      title: 'Mylo Xyloto',
-      releaseDate: {
-        year: '2011',
-        month: '10',
-        day: '24',
-      },
-      others: {
-        recordCompany: 'Capitol, Parlophone',
-        formats: 'CD, digital'
-      }
-    };
+// Ex 2
+// class App extends React.Component {
+//   render() {
+//     // Declaração do objeto contendo informações do album01
+//     const album01 = {
+//       image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+//       title: 'Mylo Xyloto',
+//       releaseDate: {
+//         year: '2011',
+//         month: '10',
+//         day: '24',
+//       },
+//       others: {
+//         recordCompany: 'Capitol, Parlophone',
+//         formats: 'CD, digital'
+//       }
+//     };
 
-    // Declaração do objeto contendo informações do album02
-    const album02 = {
-      image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
-      title: 'Ghost Stories',
-      releaseDate: {
-        year: '2014',
-        month: '05',
-        day: '16',
-      },
-      others: {
-        recordCompany: 'Parlophone',
-        formats: 'CD, digital'
-      }
-    };
+//     // Declaração do objeto contendo informações do album02
+//     const album02 = {
+//       image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+//       title: 'Ghost Stories',
+//       releaseDate: {
+//         year: '2014',
+//         month: '05',
+//         day: '16',
+//       },
+//       others: {
+//         recordCompany: 'Parlophone',
+//         formats: 'CD, digital'
+//       }
+//     };
 
-    // Retorno do que será renderizado
-    return (
-      <article>
-        <Album album={album01} />
-        <Album album={album02} />
-      </article>
-    );
-  }
+//     // Retorno do que será renderizado
+//     return (
+//       <article>
+//         <Album album={album01} />
+//         <Album album={album02} />
+//       </article>
+//     );
+//   }
+// }
+
+function App() {
+  const headphone = {
+    id: 102,
+    user: "cena@gmail.com",
+    product: "Razer Headphone",
+    price: {
+      value: 99.99,
+      currency: "dollars"
+    }
+  };
+
+  const energyDrink = {
+    id: 77,
+    user: "cena@gmail.com",
+    product: "Monster 500mL",
+    price: {
+      value: 9.99,
+      currency: "dollars"
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1> Orders recently created </h1>
+      <Order order={headphone} />
+      <Order order={energyDrink} />
+    </div>
+  );
 }
 
 export default App;
