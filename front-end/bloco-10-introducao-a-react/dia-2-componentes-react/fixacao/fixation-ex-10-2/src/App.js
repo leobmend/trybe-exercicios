9a87a2ab51d441a22f85a5cc782516e50c02435a
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-01-24 14:11:52 -0300

    Ex 2, fixacao, m2b10d2

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixacao/fixation-ex-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixacao/fixation-ex-10-2/src/App.js
@@ -1,12 +1,57 @@
 import './App.css';
-import Image from './Image'
+// import Image from './Image';
+import Album from './Album';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
-    </div>
-  );
+// Ex 1
+// function App() {
+//   // return (
+//   //   <div className="App">
+//   //     <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
+//   //   </div>
+//   // );
+// }
+
+class App extends React.Component {
+  render() {
+    // Declaração do objeto contendo informações do album01
+    const album01 = {
+      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+      title: 'Mylo Xyloto',
+      releaseDate: {
+        year: '2011',
+        month: '10',
+        day: '24',
+      },
+      others: {
+        recordCompany: 'Capitol, Parlophone',
+        formats: 'CD, digital'
+      }
+    };
+
+    // Declaração do objeto contendo informações do album02
+    const album02 = {
+      image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+      title: 'Ghost Stories',
+      releaseDate: {
+        year: '2014',
+        month: '05',
+        day: '16',
+      },
+      others: {
+        recordCompany: 'Parlophone',
+        formats: 'CD, digital'
+      }
+    };
+
+    // Retorno do que será renderizado
+    return (
+      <article>
+        <Album album={album01} />
+        <Album album={album02} />
+      </article>
+    );
+  }
 }
 
 export default App;
