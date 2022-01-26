d271f7fed4084392de2df75b42fe184cb1e034fe
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-01-26 15:57:32 -0300

    Ex 1 e 2, fixacao, m2b11d1

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fixacao/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fixacao/fancy-buttons/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const somethings = [ 'Something!', 'Sometthing!', 'Sommetthing!'];
+let index = 0;
+function consoleSomething() {
+  console.log(somethings[index]);
+  index += 1;
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onClick={consoleSomething}>Click!</button>
+        <button onClick={consoleSomething}>Click!</button>
+        <button onClick={consoleSomething}>Click!</button>
+      </div>
+    );
+  }
 }
 
 export default App;
