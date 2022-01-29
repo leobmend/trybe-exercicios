bd2d5850d25d5e45948014f776c04b4e8746d335
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-01-29 16:14:39 -0300

    Ex 6, fixacao, m2b11d2

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/fixacao/ex-form-react/src/Form.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/fixacao/ex-form-react/src/Form.jsx
@@ -25,38 +25,44 @@ class Form extends React.Component {
   render() {
     return (
       <form>
-        <label>
-          Nome:
-          <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
-        </label>
-        <label>
-          Ano de nascimento:
-          <input type="number" name="birthYear" value={this.state.birthYear} onChange={this.handleChange}/>
-        </label>
-        <label>
-          Estado:
-          <select name="state" value={this.state.state} onChange={this.handleChange}>
-            <option value=""></option>
-            <option value="GO">Goiás</option>
-            <option value="MT">Mato Grosso</option>
-            <option value="MS">Mato Grosso do Sul</option>
-            <option value="TO">Tocantins</option>
-          </select>
-        </label>
-        <label>
-          Sabe controlar os componentes React?
-          <input
-            name="controlCheck"
-            type="checkbox"
-            checked={this.state.controlCheck}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Carregue seu currículo:
-          <input type="file" />
-        </label>
-      </form>
+        <fieldset>
+          <legend>Dados pessoais</legend>
+          <label>
+            Nome:
+            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+          </label>
+          <label>
+            Ano de nascimento:
+            <input type="number" name="birthYear" value={this.state.birthYear} onChange={this.handleChange}/>
+          </label>
+          <label>
+            Estado:
+            <select name="state" value={this.state.state} onChange={this.handleChange}>
+              <option value=""></option>
+              <option value="GO">Goiás</option>
+              <option value="MT">Mato Grosso</option>
+              <option value="MS">Mato Grosso do Sul</option>
+              <option value="TO">Tocantins</option>
+            </select>
+          </label>
+        </fieldset>
+        <fieldset>
+          <legend>Dados profissionais</legend>         
+          <label>
+            Sabe controlar os componentes React?
+            <input
+              name="controlCheck"
+              type="checkbox"
+              checked={this.state.controlCheck}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Carregue seu currículo:
+            <input type="file" />
+          </label>
+        </fieldset>
+        </form>
     )
   }
 }
