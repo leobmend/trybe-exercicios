7f12553093bf44693b1869ea426e62cd8de24b2f
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-05-15 18:36:40 -0300

    Revert "Ex bonus na-pratica, m3b22d4"
    This reverts commit 2769dec407dedaea2dfd24facf89a8b2d05fcafa.

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/na-pratica/simpsons/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/na-pratica/simpsons/index.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 
-import authorization from './middlewares/authentication.js';
 import routes from './routes/index.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(authorization);
 app.use(routes);
 
 app.listen(3001, () => {
--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/na-pratica/simpsons/utils/crypto-token.js
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto';
-
-const generateToken = () => {
-  return crypto.randomBytes(8).toString('hex');
-};
-
-export default generateToken;
