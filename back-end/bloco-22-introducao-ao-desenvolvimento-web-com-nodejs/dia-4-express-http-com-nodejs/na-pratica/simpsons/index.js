5848b82cee3287ada318babb8d87a0d5e8f1b6c8
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-05-15 18:39:45 -0300

    Ex bonus na-pratica, m3b22d4

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/na-pratica/simpsons/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/na-pratica/simpsons/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 
+import authorization from './middlewares/authentication.js';
 import routes from './routes/index.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(authorization);
 app.use(routes);
 
 app.listen(3001, () => {
