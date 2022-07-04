5fff1d56f2e79bb14343e75228d5aa73ade8ba28
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-07-04 18:54:06 -0300

    Ex na-pratica fix, m3b26d1

--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/na-pratica/my-ts-scripts/dist/length.js
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/na-pratica/my-ts-scripts/dist/length.js
@@ -1,8 +1,29 @@
 "use strict";
-const lengthUnits = ["km", "hm", "dam", "m", "dm", "cm", "mm"];
-function lengthConvert(value, fromUnit, toUnit) {
-    const fromIndex = lengthUnits.indexOf(fromUnit); // pegamos o index da unidade base no array
-    const toIndex = lengthUnits.indexOf(toUnit); // pegamos o index da unidade para a conversão
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const readline_sync_1 = __importDefault(require("readline-sync")); // importamos o pacote readline-sync
+const units = ["km", "hm", "dam", "m", "dm", "cm", "mm"];
+function convert(value, fromUnit, toUnit) {
+    const fromIndex = units.indexOf(fromUnit); // pegamos o index da unidade base no array
+    const toIndex = units.indexOf(toUnit); // pegamos o index da unidade para a conversão
     const exponent = (toIndex - fromIndex); // calculamos o expoente a partir da diferença dos index
     return value * Math.pow(10, exponent);
 }
+function exec() {
+    // pegamos o valor a ser convertido digitado pela pessoa usuária
+    const value = readline_sync_1.default.questionFloat("Digite o valor a ser convertido: \n");
+    // pedimos que a pessoa usuária escolha a unidade base
+    const fromUnitChoiceIndex = readline_sync_1.default.keyInSelect(units, "Escolha um número para a unidade base:");
+    // pedimos que a pessoa usuária escolha a unidade para conversão
+    const toUnitChoiceIndex = readline_sync_1.default.keyInSelect(units, "Escolha um número para a conversão:");
+    const fromUnitChoice = units[fromUnitChoiceIndex];
+    const toUnitChoice = units[toUnitChoiceIndex];
+    const result = convert(value, fromUnitChoice, toUnitChoice);
+    // montamos a mensagem de saída
+    const message = `${value}${fromUnitChoice} é igual a ${result}${toUnitChoice}`;
+    // printamos a mensagem de saída no terminal
+    console.log(message);
+}
+exec();
