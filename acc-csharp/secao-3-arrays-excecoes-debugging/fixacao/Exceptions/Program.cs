fa61a55929d6f4cfd02edcebaea08bee04c30256
leobmend <leo.bmendonca@gmail.com>
2022-09-04 09:11:12 -0300

    Ex fixacao, c#s3l12

---
+++ acc-csharp/secao-3-arrays-excecoes-debugging/fixacao/Exceptions/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+  public static void Main()
+  {
+    string[] chemicalProduct = new string[3];
+
+    try
+    {
+    chemicalProduct[0] = "Cálcio";
+    chemicalProduct[1] = "Zinco";
+    chemicalProduct[2] = "Hidrazina";
+    chemicalProduct[3] = "Anilina"; 
+    }
+    catch (IndexOutOfRangeException ex) 
+    {
+    Console.WriteLine("Erro Específico, sabemos exatamente o motivo do erro.");
+    }
+    catch (Exception ex)
+    {
+    Console.WriteLine("Temos a mensagem, porém é um pouco incerto o que ocorreu.");
+    }
+  }
+}
