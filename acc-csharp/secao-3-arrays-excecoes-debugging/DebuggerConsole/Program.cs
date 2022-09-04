61b2fafeb09e811456c2905ad1093d3225ba0592
leobmend <leo.bmendonca@gmail.com>
2022-09-04 11:41:48 -0300

    Ex fixacao, c#s3l13

---
+++ acc-csharp/secao-3-arrays-excecoes-debugging/DebuggerConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Breakpoint 1");
+
+    Console.WriteLine("Breakpoint 2");
+
+    Console.WriteLine("Breakpoint 3");
+
+    Console.WriteLine("Breakpoint 4");
+
+    Console.WriteLine("Breakpoint 5");
+  }
+}
