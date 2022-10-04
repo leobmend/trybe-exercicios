9b48e618671b02e86c1d77f2ff11bb06e0f4c3a6
leobmend <leo.bmendonca@gmail.com>
2022-10-04 12:14:38 -0300

    Ex na-pratica, m4b35d1

---
+++ computer-science/bloco-35-redes-e-raspagem-de-dados/dia-1-arquitetura-de-redes/na-pratica/udp_server.py
@@ -0,0 +1,20 @@
+from socketserver import UDPServer, DatagramRequestHandler
+
+ADDRESS = "", 8084
+
+
+class EchoHandler(DatagramRequestHandler):
+    """Responde requisições repetindo o que foi recebido."""
+
+    def handle(self):
+        for line in self.rfile:
+            self.wfile.write(line)
+            print(line.decode("utf-8").strip())
+
+
+if __name__ == "__main__":
+    with UDPServer(ADDRESS, EchoHandler) as server:
+        server.serve_forever()
+
+# Utilize 'nc -u 127.0.0.1 8084'. Após executar o comando, digite a
+# mensagem e tecle enter para enviá-las.
